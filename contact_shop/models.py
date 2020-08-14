4da3039f195fa84ccf8722a16fb0e593dc35afde
Belca88 <you@example.com>
2020-08-14 16:16:33 +0300

    dbFix

--- contact_shop/models.py
+++ contact_shop/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from phone_field import PhoneField
 
 
 class Contact(models.Model):
     email = models.EmailField(verbose_name='Електронний адрес')
-    number_tel = models.CharField(max_length=13, verbose_name='Номер телефону')
+    number_tel = PhoneField(max_length=13, verbose_name='Номер телефону')
     address = models.CharField(max_length=30, verbose_name='Адреса')
 
     class Meta:
--- mainsite/admin.py
+++ mainsite/admin.py
@@ -1,29 +1,26 @@
 from django.contrib import admin
-# from .models import Goods, Comment, Order, MainCatalog, Catalog, PropertyImage
-#
-#
-# class GoodsDb(admin.ModelAdmin):
-#     list_display = ('name', 'price', 'details', 'order')
-#
-#
-# class CommentDb(admin.ModelAdmin):
-#     list_display = ('text', 'goods', 'user')
-#
-#
-# class OrderDb(admin.ModelAdmin):
-#     list_display = ('user', 'date')
-#
-#
-# class CatalogDb(admin.ModelAdmin):
-#     list_display = ('name', 'mainCatalog')
-#
-#
-# class PropertyImageDb(admin.ModelAdmin):
-#     list_display = ('goods', 'image')
-#
-#
-# admin.site.register(Goods, GoodsDb)
-# admin.site.register(Comment, CommentDb)
-# admin.site.register(Order, OrderDb)
-# admin.site.register(Catalog, CatalogDb)
-# admin.site.register(PropertyImage, PropertyImageDb)
+from .models import Goods, Comment, Order, MainCatalog, Catalog, PropertyImage
+
+
+class GoodsDb(admin.ModelAdmin):
+    list_display = ('name', 'price', 'details', 'catalog', 'get_images')
+
+
+class CommentDb(admin.ModelAdmin):
+    list_display = ('text', 'goods', 'user')
+
+
+class OrderDb(admin.ModelAdmin):
+    list_display = ('user', 'date', 'get_goods', 'get_count_goods')
+
+
+class CatalogDb(admin.ModelAdmin):
+    list_display = ('name', 'mainCatalog')
+
+
+admin.site.register(Goods, GoodsDb)
+admin.site.register(Comment, CommentDb)
+admin.site.register(Order, OrderDb)
+admin.site.register(Catalog, CatalogDb)
+admin.site.register(PropertyImage)
+admin.site.register(MainCatalog)
